2a3041eb2957f61bf4804ee095f75100ef076822
beherit197777 <beherit197777@gmail.com>
2023-01-17 11:09:52 +0200

    add params to controller question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,8 @@
 class QuestionsController < ApplicationController
   def create
+    Question.create(
+      body: params[:question][:body],
+      user_id: params[:question][user_id]
+    )
+  end
 end
