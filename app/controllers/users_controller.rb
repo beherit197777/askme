46de796f538fa78cbe9b8b9ea1fb1fd34f55a3be
beherit197777 <beherit197777@gmail.com>
2024-10-01 13:56:48 +0300

    add Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,67 @@
+class UsersController < ApplicationController
+  before_action :load_user, except: [:index, :create, :new]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
+  def index
+    @users = User.all
+    @tags = Tag.all.distinct.order(:name)
+  end
+
+  def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Вы успешно зарегистрировались и залогинились.'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @questions = @user.questions.order(created_at: :desc)
+
+    @new_question = @user.questions.build
+
+    @questions_count = @questions.count
+    @answers_count = @questions.where.not(answer: nil).count
+    @unanswered_count = @questions_count - @answers_count
+  end
+
+  def destroy
+    @user.destroy
+    reset_session
+    redirect_to root_url, notice: 'Ваш аккаунт успешно удален.'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url, :background_color)
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+end
