b048017d5e434d7b766c7b842f0604a11afdb620
beherit197777 <beherit197777@gmail.com>
2024-10-15 21:20:12 +0300

    edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-
+  config.hosts << "askme-0511r5ms.b4a.run"
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
 
